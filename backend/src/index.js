d762539b0801466711d4f44c16e9f354b31ea238
Nilesh Agrawal <nileshagra1998@gmail.com>
2025-06-28 13:59:42 +0530

    Fixes

--- backend/src/index.js
+++ backend/src/index.js
@@ -30,11 +30,11 @@ app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+    app.use(express.static(path.join(__dirname, '../../frontend/dist')));
 
     app.get("/*", (req, res) => {
         console.log('__dirname: ', __dirname);
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+        res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
     });
 }
 

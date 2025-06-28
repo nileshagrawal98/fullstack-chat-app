df65df23cd802958effabf22bbd452ee9241d19d
Nilesh Agrawal <nileshagra1998@gmail.com>
2025-06-28 13:57:37 +0530

    Fixes

--- backend/src/index.js
+++ backend/src/index.js
@@ -33,7 +33,8 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
     app.get("/*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+        console.log('__dirname: ', __dirname);
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
     });
 }
 
